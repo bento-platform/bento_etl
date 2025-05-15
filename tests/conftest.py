b0612d168dce4c1d0f275d4852270ba735bb2be4
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2025-05-15 11:46:28 -0400

    fix: tests env var overrides

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,12 @@ import os
 
 from fastapi.testclient import TestClient
 
+os.environ["BENTO_DEBUG"] = "true"
+os.environ["BENTO_VALIDATE_SSL"] = "false"
+os.environ["BENTO_JSON_LOGS"] = "false"  # use rich text logs in testing
 os.environ["CORS_ORIGINS"] = "*"
+
+os.environ["BENTO_AUTHZ_SERVICE_URL"] = "https://authz.local"
 os.environ["AUTHZ_ENABLED"] = "False"
 
 from bento_etl.config import Config, get_config
