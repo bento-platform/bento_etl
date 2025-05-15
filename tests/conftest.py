d5adb23a6259337b13c317229e4f7b91521d2386
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2025-05-15 11:22:04 -0400

    chore: tests

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+
+from fastapi.testclient import TestClient
+
+os.environ["CORS_ORIGINS"] = "*"
+os.environ["AUTHZ_ENABLED"] = "False"
+
+from bento_etl.config import Config, get_config
+from bento_etl.main import app
+
+@pytest.fixture
+def config() -> Config:
+    return get_config()
+
+@pytest.fixture
+def test_client():
+    with TestClient(app) as client:
+        yield client
