f2687b4b2e7ec0289bc1fb402c7c91bf56e5e85f
BarrothTheDragon <delia_bretan@yahoo.com>
2025-05-27 18:02:49 +0000

    update test to query katsu with local test json file

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,3 +1,6 @@
+import os
+import json
+import inspect
 from fastapi.testclient import TestClient
 from bento_etl.loaders.phenopackets_loader import PhenopacketsLoader
 
@@ -5,8 +8,11 @@ def test_phenopackets_loader(test_client: TestClient, logger):
     # Placeholder test
 
     loader = PhenopacketsLoader(logger)
-
-    assert loader.load("{}") == 200
-
-
-    assert True
+    
+    
+    caller_path =os.path.dirname(__file__)
+    file_path = os.path.join(caller_path, "data/synthetic_phenopackets_v2.json")
+    
+    with open(file_path) as f:
+        json_content = json.load(f)
+        assert loader.load(json_content) == 200
