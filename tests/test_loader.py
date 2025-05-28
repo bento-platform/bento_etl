ab698b71ecd0a0dfb75c76f4c54326a06e5971e2
BarrothTheDragon <delia_bretan@yahoo.com>
2025-05-28 15:17:58 +0000

    update test to send a single phenopackett and check for succcess code 204

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -6,13 +6,11 @@ from bento_etl.loaders.phenopackets_loader import PhenopacketsLoader
 
 def test_phenopackets_loader(test_client: TestClient, logger):
     # Placeholder test
-
     loader = PhenopacketsLoader(logger)
-    
     
     caller_path =os.path.dirname(__file__)
     file_path = os.path.join(caller_path, "data/synthetic_phenopackets_v2.json")
     
     with open(file_path) as f:
-        json_content = json.load(f)
-        assert loader.load(json_content) == 200
+        json_content = json.load(f)[0]
+        assert loader.load(json_content) == 204
