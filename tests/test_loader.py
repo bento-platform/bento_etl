de13f88f5f7e1de0cab4bba80964df4f3f8f9f57
BarrothTheDragon <delia_bretan@yahoo.com>
2025-05-29 18:36:21 +0000

    change load test for async

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,10 +1,10 @@
 import os
 import json
-import inspect
-from fastapi.testclient import TestClient
+import pytest
 from bento_etl.loaders.phenopackets_loader import PhenopacketsLoader
 
-def test_phenopackets_loader(test_client: TestClient, logger, config):
+@pytest.mark.anyio
+async def test_phenopackets_loader(logger, config):
     # Placeholder test
     dataset_id = "16543a4a-cee9-4e3d-9826-fcb1e0e5a292" # From bento portal; moose project
     loader = PhenopacketsLoader(logger, config, dataset_id)
@@ -14,5 +14,7 @@ def test_phenopackets_loader(test_client: TestClient, logger, config):
     file_path = os.path.join(caller_path, "data/synthetic_phenopackets_v2.json")
     
     with open(file_path) as f:
-        json_content = json.load(f)[0]
-        assert loader.load(json_content) == 204
+        json_content = json.load(f)
+        response = await loader.load(json_content)
+        assert all(r.status_code == 204 for r in response)
+
