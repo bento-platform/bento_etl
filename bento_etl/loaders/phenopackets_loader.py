bc975227fecb64d2d8d3ed8222e1422bfb0b6e69
BarrothTheDragon <delia_bretan@yahoo.com>
2025-06-02 19:41:14 +0000

    add error check and upload cancel for load phenopacket method

--- bento_etl/loaders/phenopackets_loader.py
+++ bento_etl/loaders/phenopackets_loader.py
@@ -1,6 +1,8 @@
 import httpx
 import json
 import asyncio
+from asyncio import Task
+from httpx import AsyncClient
 from fastapi import status
 from .base import BaseLoader
 
@@ -9,16 +11,31 @@ class PhenopacketsLoader(BaseLoader):
         super().__init__(logger, config)
         self.dataset_id = dataset_id
         self.batch_size = batch_size
+        self.load_url = f'http://{self.config.katsu_endpoint}/ingest/{self.dataset_id}/phenopackets_json'
 
     async def load(self, data:json):
-        katsu_ingest_url = f'http://{self.config.katsu_endpoint}/ingest/{self.dataset_id}/phenopackets_json'
+        load_requests = []
         limits = httpx.Limits(max_keepalive_connections=20, max_connections=len(data))
 
-        async with httpx.AsyncClient(limits=limits) as client:
-            #katsu_requests = [client.post(katsu_ingest_url, json=data[index: index + self.batch_size]) for index in range(0, len(data), self.batch_size)]
-            # OR
-            katsu_requests = []
+        async with AsyncClient(limits=limits) as client:
             for index in range(0, len(data), self.batch_size):
-                request = client.post(katsu_ingest_url, json=data[index: index + self.batch_size])
-                katsu_requests.append(request)
-            return await asyncio.gather(*katsu_requests)
+                batch = data[index:index + self.batch_size]
+                request = asyncio.ensure_future(self.send_request(client, self.load_url, batch))
+                load_requests.append(request)
+
+            try:
+                await asyncio.gather(*load_requests)
+            except Exception as ex:
+                self.logger.warning("Cancelling all Phenopacket uploads")
+                self.cancel_all_requests(load_requests)
+
+    async def send_request(self, client:AsyncClient, request_url:str, data:json):
+        request = await client.post(request_url, json=data)
+        if request.status_code != status.HTTP_204_NO_CONTENT:
+            error_message = f"Phenopacket upload to Katsu failed with error {request.status_code}"
+            self.logger.error(error_message)
+            raise Exception(error_message)
+
+    def cancel_all_requests(self, requests:list[Task]):
+        for request in requests:
+            request.cancel()
