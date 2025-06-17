1207ea63f57d9da36adc996c3f5347784fc26ba5
David <do-not-reply@example.org>
2025-06-17 19:30:13 +0000

    retrieve client id from env

--- bento_etl/config.py
+++ bento_etl/config.py
@@ -22,6 +22,7 @@ class Config(BentoFastAPIBaseConfig):
     )
     katsu_url: str = os.getenv("KATSU_URL", "http://localhost:8000")
     bento_validate_ssl: bool = os.getenv("BENTO_VALIDATE_SSL", True)
+    etl_client_id: str = os.getenv("ETL_CLIENT_ID", "")
     etl_client_secret: str = os.getenv("ETL_CLIENT_SECRET", "")
     openid_config_url: str = os.getenv("BENTO_OPENID_CONFIG_URL", "https://bentov2auth.local/realms/bentov2/.well-known/openid-configuration")
 
