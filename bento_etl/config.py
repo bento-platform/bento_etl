824235a893e830d0bb87aa882e61b8fbd8f37228
BarrothTheDragon <delia_bretan@yahoo.com>
2025-06-19 16:28:14 +0000

    refact config file

--- bento_etl/config.py
+++ bento_etl/config.py
@@ -20,13 +20,12 @@ class Config(BentoFastAPIBaseConfig):
     service_description: str = (
         "ETL service for external data ingestion into the Bento platform"
     )
-    katsu_url: str = os.getenv("KATSU_URL", "http://localhost:8000")
-    bento_validate_ssl: bool = os.getenv("BENTO_VALIDATE_SSL", True)
-    etl_client_id: str = os.getenv("ETL_CLIENT_ID", "")
-    etl_client_secret: str = os.getenv("ETL_CLIENT_SECRET", "")
-    openid_config_url: str = os.getenv("BENTO_OPENID_CONFIG_URL", "https://bentov2auth.local/realms/bentov2/.well-known/openid-configuration")
 
     # Service Configs
+    katsu_url: str = "http://localhost:8000"
+    etl_client_id: str = ""
+    etl_client_secret: str = ""
+    openid_config_url: str = "https://bentov2auth.local/realms/bentov2/.well-known/openid-configuration"
 
 
 @lru_cache
