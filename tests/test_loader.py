1992fcc69917a0225129f885d031af2bac65787b
David <do-not-reply@example.org>
2025-06-23 19:59:56 +0000

    wip: mock using monkeypatch

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,29 +2,38 @@ import uuid
 import httpx
 import pytest
 from unittest.mock import patch
+from bento_etl import authz
 from bento_etl.loaders.phenopackets_loader import PhenopacketsLoader
 
 
 class TestPhenopacketsLoader:
-    async_client_path = "bento_etl.loaders.phenopackets_loader.httpx.AsyncClient.post"
-
+    @pytest.fixture
+    def mock_response(self, monkeypatch):
+        def mock_get(*args, **kwargs):
+            return ""
+        monkeypatch.setattr(authz, "get_bearer_token", mock_get)
+        
+        async def mockreturn2(*args, **kwargs):
+                    return httpx.Response(204)
+        monkeypatch.setattr(httpx.AsyncClient, "post", mockreturn2)
+    
+    
+    
+    # TODO test for batch_size
     @pytest.mark.asyncio
-    @patch(async_client_path, return_value=httpx.Response(204))
-    async def test_valid_load(self, logger, config):
+    async def test_valid_load(self, logger, config, mock_response):
         loader = PhenopacketsLoader(logger, config, uuid.uuid4(), 4)
         await loader.load("{}")
 
     @pytest.mark.asyncio
-    @patch(async_client_path, return_value=httpx.Response(400))
-    async def test_invalid_dataset_id(self, logger, config):
+    async def test_invalid_dataset_id(self, logger, config, mock_response):
         loader = PhenopacketsLoader(logger, config, "BAD_DATASET_ID", 4)
 
         with pytest.raises(Exception, match="400"):
             await loader.load("{}")
 
     @pytest.mark.asyncio
-    @patch(async_client_path, return_value=httpx.Response(400))
-    async def test_invalid_data(self, logger, config):
+    async def test_invalid_data(self, logger, config, mock_response):
         loader = PhenopacketsLoader(logger, config, uuid.uuid4(), 4)
 
         with pytest.raises(Exception, match="400"):
