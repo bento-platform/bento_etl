b8d8c49dfa3d1a7a75095d993a4612f7678bf1a0
BarrothTheDragon <delia_bretan@yahoo.com>
2025-06-25 15:59:17 +0000

    correctly call load function

--- bento_etl/loaders/phenopackets_loader.py
+++ bento_etl/loaders/phenopackets_loader.py
@@ -1,10 +1,4 @@
-import httpx
 import json
-import asyncio
-from asyncio import Task
-from httpx import AsyncClient
-
-from bento_etl import authz
 from .base import BaseLoader
 
 
@@ -13,12 +7,11 @@ class PhenopacketsLoader(BaseLoader):
         super().__init__(logger, config)
         if batch_size < 0:
             raise ValueError("Batch size must be at least 0")
-        self.dataset_id = dataset_id
         self.batch_size = batch_size
         self.load_url = (
-            f"{self.config.katsu_url}ingest/{self.dataset_id}/phenopackets_json"
+            f"{self.config.katsu_url}ingest/{dataset_id}/phenopackets_json"
         )
-        
-    async def load(self, data):
-        await self._load_json(data)
+
+    async def load(self, data:list):
+        await self._load(data, self.load_url, self.batch_size)
 
