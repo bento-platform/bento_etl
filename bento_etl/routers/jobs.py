72e1cfb88f9c0f2e7de2f1f168e09269329e5be0
noctillion <ornatus30@gmail.com>
2025-07-28 16:40:58 -0400

    temp_hack

--- bento_etl/routers/jobs.py
+++ bento_etl/routers/jobs.py
@@ -63,6 +63,14 @@ async def submit_job(
     loader: LoaderDep,
     db: JobStatusDatabaseDependency,
 ):
+    import os
+    import json
+    batch_size = 4
+    caller_path =os.path.dirname(__file__)
+    file_path = os.path.join(caller_path, "phenopackets_result.json")
+    with open(file_path) as f:
+        json_content = json.load(f)
+        response = await loader.load(json_content)
     job_id = db.create_status(job.model_dump()).id
     bt.add_task(run_pipeline, job_id, extractor, transformer, loader, db)
     return {"message": f"Running ETL job in the background {job_id}"}
