1c88b45ab2ed9a1ec7424eb6cd21d181a5f0baf4
noctillion <ornatus30@gmail.com>
2025-09-17 18:09:57 -0400

    add mapping paths to conf

--- bento_etl/config.py
+++ bento_etl/config.py
@@ -2,6 +2,7 @@ from bento_lib.config.pydantic import BentoFastAPIBaseConfig
 from fastapi import Depends
 from functools import lru_cache
 from typing import Annotated
+import os
 
 from .constants import SERVICE_GROUP, SERVICE_ARTIFACT
 
@@ -25,6 +26,22 @@ class Config(BentoFastAPIBaseConfig):
     etl_client_id: str = ""
     etl_client_secret: str = ""
     bento_openid_config_url: str = ""
+
+    # Mapping paths phenopackets
+    phenopackets_mappings_path: str =  os.path.join(
+        os.path.dirname(__file__), "transformers/plugins/phenopackets/mappings/pcgl_phenopackets_mappings.json"
+    )
+    phenopackets_ontology_mappings_path: str =  os.path.join(
+        os.path.dirname(__file__), "transformers/plugins/phenopackets/mappings/pcgl_phenopackets_ontology_mappings.json"
+    )
+
+    # Mapping paths experimets
+    experiments_mappings_path: str =  os.path.join(
+        os.path.dirname(__file__), "transformers/plugins/experiments/mappings/pcgl_experiments_mappings.json"
+    )
+    experiments_ontology_mappings_path: str =  os.path.join(
+        os.path.dirname(__file__), "transformers/plugins/experiments/mappings/pcgl_experiments_ontology_mappings.json"
+    )
 
 
 @lru_cache
