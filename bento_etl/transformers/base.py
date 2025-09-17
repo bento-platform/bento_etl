346f2c2c8db14fc4609666c782791404c7a32ac3
noctillion <ornatus30@gmail.com>
2025-09-17 18:13:21 -0400

    rm polar

--- bento_etl/transformers/base.py
+++ bento_etl/transformers/base.py
@@ -1,26 +1,19 @@
-import polars as pl
 from logging import Logger
+from typing import Any
 
 __all__ = ["BaseTransformer"]
-
 
 class BaseTransformer:
     """
     Base class for ETL transformer implementation.
-
     Transformers read the raw data from an Extractor, transform it to the desired format
     and then forward it to a Loader.
     """
-
     def __init__(self, logger: Logger):
         self.logger = logger
 
-    def transform(
-        self, raw: pl.DataFrame | pl.LazyFrame
-    ) -> pl.DataFrame | pl.LazyFrame:
-        # TODO: figure out best return type hint
+    def transform(self, raw: Any) -> Any:
         raise NotImplementedError
-
 
 # TODO: implement Phenopacket and Experiment transformers that take in PCGL JSON data.
 
