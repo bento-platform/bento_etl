5329a7d221d9c9568f538b1dc8c1d745e64f412d
noctillion <do-not-reply@example.org>
2025-09-18 14:53:14 +0000

    lint

--- bento_etl/transformers/plugins/experiments/experiments_transformer.py
+++ bento_etl/transformers/plugins/experiments/experiments_transformer.py
@@ -1,4 +1,3 @@
-from typing import Any
 from logging import Logger
 import json
 
--- bento_etl/transformers/plugins/phenopackets/utils.py
+++ bento_etl/transformers/plugins/phenopackets/utils.py
@@ -1,4 +1,3 @@
-import json
 from collections import defaultdict
 from jsonschema import validate, ValidationError
 from typing import Any
